58daac0e45ef611f248c70f13c05c45765b2d79c
fz <natureorz@foxmail.com>
2016-05-17 11:57:17 +0800

    add comment

--- src/main/java/com/qrcode/QRCodeUtil.java
+++ src/main/java/com/qrcode/QRCodeUtil.java
@@ -18,16 +18,24 @@ import java.util.Hashtable;
  */
 public class QRCodeUtil {
 
+    // 二维码属性
     private static Hashtable ht = new Hashtable(){{
         put(EncodeHintType.CHARACTER_SET, "utf-8");
+        put(EncodeHintType.MARGIN, 1);
     }};
 
+    // 黑白像素
+    private static final int BLACK = 0xFF000000;
+    private static final int WHITE = 0xFFFFFFFF;
+
+    // bit矩阵 转换为 缓存图片
     public static BufferedImage createQRCodePngImg(String url, int width, int height) throws Exception {
 
         BitMatrix bitMatrix = new MultiFormatWriter().encode(url, BarcodeFormat.QR_CODE, width, height, ht);
         return toBufferedImage(bitMatrix);
     }
 
+    // 将bit矩阵 转换为 黑白像素
     private static BufferedImage toBufferedImage(BitMatrix matrix) {
 
         int width = matrix.getWidth();
@@ -35,7 +43,7 @@ public class QRCodeUtil {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                image.setRGB(x, y, matrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+                image.setRGB(x, y, matrix.get(x, y) ? BLACK : WHITE);
             }
         }
         return image;
@@ -43,14 +51,14 @@ public class QRCodeUtil {
 
     public static void easyQRCodePrint(String url) throws Exception {
 
-        int width = 120;
-        int height = 120;
+        int width = 65;
+        int height = 65;
         int x = 0;
         int y = 0;
         BufferedImage image = createQRCodePngImg(url, width, height);
         PngPrintable pngPrintable = new PngPrintable(image, x, y, width, height);
         PrintRequestAttributeSet pras = new HashPrintRequestAttributeSet();
-        pras.add(new MediaPrintableArea(0f, 0f, width/72f, height/72f, MediaPrintableArea.INCH));
+        pras.add(new MediaPrintableArea(0f, 0f, width, height, MediaPrintableArea.INCH));
         PrintUtil.printPrintable(pras, pngPrintable);
     }
 
